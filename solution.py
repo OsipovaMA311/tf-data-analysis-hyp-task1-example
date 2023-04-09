8ac669336479210d7adffe02bab75e37aedc0fc2
marina_a_o <82184941+OsipovaMA311@users.noreply.github.com>
2023-04-09 19:36:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 468819439 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,8 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    count = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    stat, pval = proportions_ztest(count, nobs, alternative='smaller')
+    return (pval < 0.08) # Ваш ответ, True или False
